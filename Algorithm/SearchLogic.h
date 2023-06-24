01df2436436bb479a7a8ae4169bc8799a58cafb7
QUOTIENT <tjsgh5965@naver.com>
2023-06-24 23:46:39 +0900

    linearSearch, BinarySearch

---
+++ Algorithm/SearchLogic.h
@@ -0,0 +1,29 @@
+#pragma once
+
+template <typename T>
+int linearSearch(T arr[], T value) {
+	for (int i = 0; i < sizeof(arr) / sizeof(T); i++) {
+		if (arr[i] == value) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+int binarySearch(int arr[], int target) {
+	
+	int low = 0;
+	int high = sizeof(arr) / sizeof(int) - 1;
+
+	while (low <= high)
+	{
+		int middle = low + (high - low) / 2;
+		int value = arr[middle];
+
+		if (value < target) low = middle + 1;
+		else if (value > target) high = middle + 1;
+		else return middle;
+	}
+
+	return -1;
+}
